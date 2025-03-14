bed1be2eedfb0f273cc93c2904759a827ad99529
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-03-15 03:43:54 +0600

    2 sec dn

---
+++ Task-3(Portfolio)/about_sec.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let aboutSection = document.querySelector("#about");
+//     aboutSection.style.opacity = "0";
+//     aboutSection.style.transition = "opacity 1.5s ease-in-out";
+
+//     window.addEventListener("scroll", function () {
+//         let position = aboutSection.getBoundingClientRect().top;
+//         let screenHeight = window.innerHeight;
+//         if (position < screenHeight - 100) {
+//             aboutSection.style.opacity = "1";
+//         }
+//     });
+// });
